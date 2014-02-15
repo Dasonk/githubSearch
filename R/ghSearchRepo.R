c14f24cc51856f3e6dd680c5b87e25b361d763fb
Dason Kurkiewicz <dasonk@iastate.edu>
2014-02-15 16:41:40 -0600

    Added user and language parameters to ghSearchRepo

--- R/ghSearchRepo.R
+++ R/ghSearchRepo.R
@@ -4,6 +4,8 @@
 #' Search for repo on github
 #' 
 #' @param repo Character. Name of repo
+#' @param user character. If you want to only search for repositories for a specific user.
+#' @param language character. If you want to specify a programming language for the repository.
 #' @param only.fullname Logical.  Should only the full name in the form "user/repo"
 #'  of the results be returned?  If TRUE then only the full_name is returned, if FALSE then
 #'  the entire JSON is returned as a list.
@@ -11,12 +13,28 @@
 #' @import RJSONIO
 #' @export
 #' @examples
+#' \dontrun{
+#' # Requires internet connection
 #' ghSearchRepo("pacman")
 #' ghSearchRepo("Dmisc")
 #' ghSearchRepo("ggplot2")
-ghSearchRepo <- function(repo, only.fullname = TRUE){
-    srch <- paste0("https://api.github.com/search/repositories?q=", repo, "+language:R")
-    response <- getURL(srch, ssl.verifypeer = FALSE, useragent = "RCurl")
+#' 
+#' # Get a list of packages by "Dasonk"
+#' ghSearchRepo("", user = "Dasonk")
+#' }
+ghSearchRepo <- function(repo, user = NULL, language = "R", only.fullname = TRUE){
+    
+    if(!is.null(user)){
+        user <- paste0("+user:", user)
+    }
+    if(!is.null(language)){
+        language <- paste0("+language:", language)
+    }
+    search <- paste0("https://api.github.com/search/repositories?q=", 
+                   repo, 
+                   user, 
+                   language)
+    response <- getURL(search, ssl.verifypeer = FALSE, useragent = "RCurl")
     output <- fromJSON(response)
     if(output[["total_count"]] == 0){
         return(NULL)
